86773a876e9af8dd73fd6263bc15fd5bec73cdd6
Younes <ylmourab77@gmail.com>
2021-01-19 09:52:02 +0100

    fin exo14

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React,{useState} from 'react'
+import Composant from './components/Composant'
+
 
 
 function App() {
   const [titre,setTitre] = useState("Bonjour")
+  const [test,setTest] = useState("yo")
 
   return (
     <div className="App">
       <h1>{titre}</h1>
+      <Composant nom={test}></Composant>
+  
+      
      
     </div>
   );
---
+++ src/components/Composant.js
@@ -0,0 +1,14 @@
+import React,{useState} from 'react'
+
+function Composant(props) {
+
+    
+    return (
+        <div>
+            <h1>{props.nom}</h1>
+            
+        </div>
+    )
+}
+
+export default Composant
